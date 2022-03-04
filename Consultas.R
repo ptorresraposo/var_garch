3dbae9c8fd9508b6c96aa419f3d72171b351233f
ptorresraposo <patotorres@gmail.om>
2022-03-04 12:07:36 -0300

    modificaciones

--- Consultas.R
+++
@@ -1,9 +0,0 @@
-
-prv <- fromJSON(txt="https://fintual.cl/api/asset_providers")
-ca <- fromJSON(txt="https://fintual.cl/api/conceptual_assets")
-View(ca)
-
-
-# model = c('sGARCH','eGARCH','gjrGARCH','apARCH','iGARCH','fGARCH','fGARCH','fGARCH','fGARCH')
-# submodel = c(NA, NA, NA, NA, NA, 'TGARCH', 'AVGARCH', 'NGARCH', 'NAGARCH')  
-#distr= c('norm', 'std', 'ged', 'nig', 'jsu')
